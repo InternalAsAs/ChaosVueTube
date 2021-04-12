2fadb370a67ef9ee0c052a5d8fc064770daf1c1f
東毅幸 <azumatakeyuki@azumatakeshikounoMacBook-Pro.local>
2021-04-12 20:23:26 +0900

    first commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,6 @@
             </a>
         </h1>
         <p id="sub-title">{{ m.name }}！サムネは投稿日順で、再生数が多いほど大きく表示されます。</p>
-        <p style="color: white;" ><strong>サムネクリックで動画へジャンプ♪</strong></p>
     </header>
   </div>
 </template>
