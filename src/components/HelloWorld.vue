7a04e7501707225c0e8bec20720ef8c1cf061ba3
東毅幸 <azumatakeyuki@azumatakeshikounoMacBook-Pro.local>
2021-04-14 22:34:14 +0900

    add search

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@
         </h1>
       </Header>
         <p style="color: white; font-size: smaller;">Welcome to chaostic YouTube！<br>チャンネル内平均再生回数と動画再生回数を相関させてサムネ敷き詰め画を生成しています！</p>
-        <input id="search" type="text" placeholder="search" v-model="keyword">
+        <input id="search" type="text" placeholder="search" v-model="keyword" onclick="javascript:void(0);">
             <div class="container" style="display: flex; flex-wrap: wrap; justify-content: center;">
               <div v-for="m of filteredResults" :key="m.id" class="card-seed" style="margin: 10px;">
                   <div class="card-wrapper">
