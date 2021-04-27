eecee53fc0fbe28ebbb38877d63f8626f41d5f6a
東毅幸 <azumatakeyuki@azumatakeshikounoMacBook-Pro.local>
2021-04-27 17:11:30 +0900

    description change

---
+++ src/views/daigo.vue
@@ -0,0 +1,38 @@
+
+<template>
+<div>
+  <Loading v-if="show"/>
+  <Header :path="this.$route.query.path"/>
+  <daigomain/>
+</div>
+</template>
+
+<script>
+import Header from '../components/Header'
+import Loading from '../components/Loading'
+
+export default {
+  name: 'daigo',
+  data: () => ({
+    path: String,
+    show: true
+  }),
+  mounted () {
+    setTimeout(() => {
+      this.show = false
+    }, 2000)
+  },
+  components: {
+    Loading,
+    Header,
+    daigomain: () => import('../components/Body/daigomain.vue')
+  }
+}
+</script>
+
+<style scoped>
+#main-pic {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/views/kajisac.vue
@@ -0,0 +1,38 @@
+
+<template>
+<div>
+  <Loading v-if="show"/>
+  <Header :path="this.$route.query.path"/>
+  <kajisacmain/>
+</div>
+</template>
+
+<script>
+import Header from '../components/Header'
+import Loading from '../components/Loading'
+
+export default {
+  name: 'kajisac',
+  data: () => ({
+    path: String,
+    show: true
+  }),
+  mounted () {
+    setTimeout(() => {
+      this.show = false
+    }, 2000)
+  },
+  components: {
+    Loading,
+    Header,
+    kajisacmain: () => import('../components/Body/kajisacmain.vue')
+  }
+}
+</script>
+
+<style scoped>
+#main-pic {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
