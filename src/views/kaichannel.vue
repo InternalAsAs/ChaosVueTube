e51a01677becb62cb049f9ccdae9f885a1d45c2b
東毅幸 <azumatakeyuki@azumatakeshikounoMacBook-Pro.local>
2021-05-11 14:17:30 +0900

    add

---
+++ src/views/kaichannel.vue
@@ -0,0 +1,38 @@
+
+<template>
+<div>
+  <Loading v-if="show"/>
+  <Header :path="this.$route.query.path"/>
+  <kaichannelmain/>
+</div>
+</template>
+
+<script>
+import Header from '../components/Header'
+import Loading from '../components/Loading'
+
+export default {
+  name: 'kaichannel',
+  data: () => ({
+    path: String,
+    show: true
+  }),
+  mounted () {
+    setTimeout(() => {
+      this.show = false
+    }, 2000)
+  },
+  components: {
+    Loading,
+    Header,
+    kaichannelmain: () => import('../components/Body/kaichannelmain.vue')
+  }
+}
+</script>
+
+<style scoped>
+#main-pic {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
